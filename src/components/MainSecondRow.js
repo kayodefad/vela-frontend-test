db44cc5dac018684248534eb9024a684a031bbfe
Oluwakayode <fadojuk@gmail.com>
2020-01-24 19:45:46 +0100

    Completed big graph

--- src/components/MainSecondRow.js
+++ src/components/MainSecondRow.js
@@ -1,66 +1,38 @@
 import React from 'react'
 import './MainSecondRow.css'
-import Jan2018 from '../images/BodySecondLayer/SecondLayerLeft/Jan2018.png'
-import Apr from '../images/BodySecondLayer/SecondLayerLeft/Apr.png'
-import biggrapgh from '../images/BodySecondLayer/SecondLayerLeft/biggrapgh.png'
-import Feb from '../images/BodySecondLayer/SecondLayerLeft/Feb.png'
-import Jan from '../images/BodySecondLayer/SecondLayerLeft/Jan.png'
-import jan2june from '../images/BodySecondLayer/SecondLayerLeft/jan2june.png'
-import Jun from '../images/BodySecondLayer/SecondLayerLeft/Jun.png'
-import LeftButton from '../images/BodySecondLayer/SecondLayerLeft/LeftButton.png'
-import leftArrow from '../images/BodySecondLayer/SecondLayerLeft/leftArrow.png'
-import Mar from '../images/BodySecondLayer/SecondLayerLeft/Mar.png'
-import May from '../images/BodySecondLayer/SecondLayerLeft/May.png'
-import rightArrow from '../images/BodySecondLayer/SecondLayerLeft/rightArrow.png'
-import RightButton from '../images/BodySecondLayer/SecondLayerLeft/RightButton.png'
-import secondLayerRectangle from '../images/BodySecondLayer/SecondLayerLeft/secondLayerRectangle.png'
 import green from '../images/BodySecondLayer/SecondLayerRight/green.png'
 import yellow from '../images/BodySecondLayer/SecondLayerRight/yellow.png'
 
 const MainSecondRow = () => {
-  const style = {
-    background: '#fff',
-    padding: '2px 7px',
-    border: '#bdbaba',
-    borderRadius: '5px'
-  }
   return (
     <div className="MainSecondRow">
-      {/* <div className="topofgraph">
-        <img className="today" src={Jan2018} alt="Jan2018" />
-        <img src={jan2june} alt="jan2june" />
-        <div className="arrows">
-          <img
-            style={{ marginRight: '13px' }}
-            className="leftArrow"
-            src={leftArrow}
-            alt="leftArrow"
-          />
-          <img className="rightArrow" src={rightArrow} alt="rightArrow" />
-        </div>
-      </div>
-      <div className="biggraph">
-        <img src={Jan} alt="Jan" />
-        <img src={Feb} alt="Feb" />
-        <img src={Mar} alt="Mar" />
-        <img src={Apr} alt="Apr" />
-        <img src={May} alt="May" />
-        <img src={Jun} alt="Jun" />
-      </div> */}
       <div className="leftbar">
-        <div>
-          <h3>Today: 5, Aug 2018</h3>
-          <div class="custom-select">
-            <select>
-              <option value="0">Select car:</option>
-              <option value="1">Audi</option>
-              <option value="2">BMW</option>
+        <div className="Daterow">
+          <h3 style={{ fontWeight: '0.7rem' }}>Today: 5, Aug 2018</h3>
+          <div className="custom-select">
+            <select className="select-date">
+              <option value="0">1 Jan - 1 Jun</option>
+              <option value="1">1 Jul - 1 Dec</option>
+              <option value="2">1 Jan - 1 Jun</option>
             </select>
           </div>
-          <div>
-            <i className="fas fa-chevron-left"></i>
+          <div className="arrows">
+            <i
+              style={{ marginRight: '10px' }}
+              className="fas fa-chevron-left"
+            ></i>
             <i className="fas fa-chevron-right"></i>
           </div>
+        </div>
+        <div className="biggraph">
+          {/* <div className="months"> */}
+          <span style={{ position: 'absolute', left: '22px' }}>Jan</span>
+          <span style={{ position: 'absolute', left: '147px' }}>Feb</span>
+          <span style={{ position: 'absolute', left: '260px' }}>Mar</span>
+          <span style={{ position: 'absolute', left: '378px' }}>Apr</span>
+          <span style={{ position: 'absolute', left: '495px' }}>May</span>
+          <span style={{ position: 'absolute', left: '615px' }}>Jun</span>
+          {/* </div> */}
         </div>
       </div>
       <div className="rightbar">
