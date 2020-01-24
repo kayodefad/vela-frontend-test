563c559a60dd1377bcddc1a958be0993f8002547
Oluwakayode <fadojuk@gmail.com>
2020-01-24 10:58:14 +0100

    Added Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import header from '../images/Header/topMenu.png'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <img src={header} alt="Header" />
+    </div>
+  )
+}
+
+export default Header
