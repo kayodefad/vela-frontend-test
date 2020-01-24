43129e09293f54261f8cd2f0a27b3b1db12fb767
Oluwakayode <fadojuk@gmail.com>
2020-01-24 15:16:13 +0100

    Work continues

--- src/components/MainFirstRow.js
+++ src/components/MainFirstRow.js
@@ -3,16 +3,17 @@ import dailytransactionvol from '../images/BodyFirstLayer/dailytransactionvol.pn
 import DailyTransactionValue from '../images/BodyFirstLayer/DailyTransactionValue.png'
 import TotalTransVol from '../images/BodyFirstLayer/TotalTransVol.png'
 import TotalTransVal from '../images/BodyFirstLayer/TotalTransVal.png'
+import './MainFirstRow.css'
 
 const MainFirstRow = () => {
   return (
     <div className="MainFirstRow">
       <div className="value">
-        <img src={dailytransactionvol} alt={dailytransactionvol} />
+        <img style={{marginRight:'5px'}} src={dailytransactionvol} alt={dailytransactionvol} />
         <img src={DailyTransactionValue} alt={DailyTransactionValue} />
       </div>
       <div className="volume">
-        <img src={TotalTransVol} alt={TotalTransVol} />
+        <img style={{marginRight:'5px'}} src={TotalTransVol} alt={TotalTransVol} />
         <img src={TotalTransVal} alt={TotalTransVal} />
       </div>
     </div>
