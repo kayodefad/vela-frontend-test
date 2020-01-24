f968c3ddc6975eebf4a8753e9f2523ef2fa66ed8
Oluwakayode <fadojuk@gmail.com>
2020-01-24 11:05:25 +0100

    Working on the SideBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from 'react'
 import Header from './Header'
-import './App.css';
+import SideBar from './SideBar'
+import Main from './Main'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="BodyContainer">
+        <SideBar />
+        <Main />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
