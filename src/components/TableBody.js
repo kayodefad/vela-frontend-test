bd230be24e460b81dba0c30bc6ebcbc57efc9982
Oluwakayode <fadojuk@gmail.com>
2020-01-25 04:54:41 +0100

    Completed table

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -1,95 +1,75 @@
 import React from 'react'
 import './TableBody.css'
 import { itemsPurchased } from '../itemsPurchased'
-import ItemDetail from './ItemDetail'
 import vwsymbol from '../images/BodyTable/vwsymbol.png'
 import pendcirc from '../images/BodyTable/pendcirc.png'
 import reconicon from '../images/BodyTable/reconicon.png'
 import unreconcirc from '../images/BodyTable/unreconcirc.png'
 
-const renderItems = () => {
-  return itemsPurchased.map(item => {
-    return <tr>
-    <td className="prac">
-      <img src={vwsymbol} alt="vwsymbol" />
-      <span>{item.itemType}</span>
-    </td>
-    <td>{item.price}</td>
-    <td>{item.transNo}</td>
-    <td>{item.time}</td>
-    <td>
-      <span className="recon">
-        <img src={reconicon} alt="reconicon" /> <span>{item.status}</span>
-      </span>
-    </td>
-    <td>
-      <i className="fas fa-chevron-down"></i>
-    </td>
-  </tr>
-  })
-}
+class TableBody extends React.Component {
+  renderItems = () => {
+    return itemsPurchased.map(item => {
+      return (
+        <tr key={item.id}>
+          <td className="prac">
+            <img src={vwsymbol} alt="vwsymbol" />
+            <span>{item.itemType}</span>
+          </td>
+          <td>{item.price}</td>
+          <td>{item.transNo}</td>
+          <td>{item.time}</td>
+          <td>
+            <div
+              className="recon"
+              style={{
+                color:
+                  item.status === 'Reconciled'
+                    ? '#19aa19'
+                    : item.status === 'Pending'
+                    ? '#e4e445'
+                    : '#868484'
+              }}
+            >
+              <img
+                src={
+                  item.status === 'Reconciled'
+                    ? reconicon
+                    : item.status === 'Pending'
+                    ? pendcirc
+                    : unreconcirc
+                }
+                alt="status icon"
+              />{' '}
+              <span>{item.status}</span>
+            </div>
+          </td>
+          <td>
+            <i className="fas fa-chevron-down"></i>
+          </td>
+        </tr>
+      )
+    })
+  }
 
-const TableBody = () => {
-  return (
-    <div>
-      <table className="TableBody">
-        <thead>
-          <tr>
-            <th>Item Type</th>
-            <th>Price</th>
-            <th>Transaction No</th>
-            <th>Time</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {renderItems()}
-          {/* <tr>
-            <td className="prac">
-              <img src={vwsymbol} alt="vwsymbol" />
-              <span>Oluwakayode Fad</span>
-            </td>
-            <td>Maria Anders</td>
-            <td>Germany</td>
-            <td>12:30</td>
-            <td>
-              <span className="recon">
-                <img src={reconicon} alt="reconicon" /> <span>Reconciled</span>
-              </span>
-            </td>
-            <td>
-              <i className="fas fa-chevron-down"></i>
-            </td>
-          </tr> */}
-          {/* <tr>
-            <td>Berglunds snabbköp</td>
-            <td>Christina Berglund</td>
-            <td>Sweden</td>
-            <td>12:30</td>
-            <td>
-              <img src={unreconcirc} alt="unreconcirc" /> Pending
-            </td>
-            <td>
-              <i className="fas fa-chevron-down"></i>
-            </td>
-          </tr>
-          <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>Francisco Chang</td>
-            <td>Mexico</td>
-            <td>12:30</td>
-            <td>
-              <img src={pendcirc} alt="pendcirc" /> Un-reconciled
-            </td>
-            <td>
-              <i className="fas fa-chevron-down"></i>
-            </td>
-          </tr> */}
-        </tbody>
-      </table>
-    </div>
-  )
+  render() {
+    return (
+      <div>
+        <table className="TableBody">
+          <thead>
+            <tr>
+              <th>Item Type</th>
+              <th>Price</th>
+              <th>Transaction No</th>
+              <th>Time</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>{this.renderItems()}</tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default TableBody
