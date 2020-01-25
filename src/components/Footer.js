a11067407c2ffe9f8f7b44d7b418b191eea0084d
Oluwakayode <fadojuk@gmail.com>
2020-01-25 05:16:26 +0100

    Added the footer

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <div>Showing 1 to 10 of 500 entries</div>
+      <div className="navigation">
+        <div>Previous</div>
+        <div>1</div>
+        <div>2</div>
+        <div>Next</div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import MainFirstRow from './MainFirstRow'
 import MainSecondRow from './MainSecondRow'
 import Table from './Table'
+import Footer from './Footer'
 
 const Main = () => {
   return (
@@ -9,6 +10,7 @@ const Main = () => {
       <MainFirstRow />
       <MainSecondRow />
       <Table />
+      <Footer />
     </div>
   )
 }
